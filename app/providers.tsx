e1afecd9b7b807799d1dae99c06860b27f3f16eb
ghefraim <ghiurauefraim@gmail.com>
2024-09-07 22:54:46 +0300

    Add debugging logs for Auth error 8

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { useState } from "react";
+import { Suspense, useState } from "react";
 
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/sonner";
+import { LoadingIcon } from "@/components/LoadingIcon";
 
 export default function Providers({ children }) {
   const [queryClient] = useState(() => new QueryClient());
@@ -13,7 +14,7 @@ export default function Providers({ children }) {
     <QueryClientProvider client={queryClient}>
       {/* TODO: Switch to defualt theme "system" */}
       <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-        {children}
+        <Suspense fallback={<LoadingIcon />}>{children}</Suspense>
       </ThemeProvider>
       <Toaster />
     </QueryClientProvider>
