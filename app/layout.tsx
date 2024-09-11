6c6276891c969bac9328feae02222744c8682ed4
ghefraim <ghiurauefraim@gmail.com>
2024-09-11 17:36:44 +0300

    Fixed background grid

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
           budge.variable
         )}
       >
-        <div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]">
+        <div className="bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]">
           <Providers>{children}</Providers>
         </div>
       </body>
